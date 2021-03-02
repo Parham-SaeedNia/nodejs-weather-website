2fe9885888c313bed734724c149a36878a16f634
Parham <parham@devtw.in>
2021-03-02 13:26:35 +0330

    Humidity added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
                     callback('Unable to find location', undefined)
                 } else {
                     callback(undefined,body.current.weather_descriptions[0]+' It is currently '+body.current.temperature+
-                    ' degrees but it feels like ' + 'And the humidity is '+body.current.humidity + '%  ' +body.current.feelslike +': '+body.location.region)
+                    ' degrees but it feels like ' +body.current.feelslike + ' And the humidity is '+body.current.humidity + '%')
                 }
     })
 }
